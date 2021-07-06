a3b65ed3ff44c539609ef432cfac1e173b87a794
ktyl <me@ktyl.dev>
2021-07-06 23:13:56 +0100

    bump opengl version

--- res/shader/shader.vert
+++ res/shader/shader.vert
@@ -1,4 +1,4 @@
-#version 330 core
+#version 430 core
 layout (location = 0) in vec3 aPos;         // position has attribute position 0
 layout (location = 1) in vec3 aColor;       // color has attribute position 1
 layout (location = 2) in vec2 aTexCoord;    // texture coordinate
--- src/gfx.c
+++ src/gfx.c
@@ -21,8 +21,8 @@ void gfxInit()
     }
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
     sdlWindow = SDL_CreateWindow(

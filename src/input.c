3b7d413390179419a6a472f4e2aa9356edbb8b52
ktyl <me@ktyl.dev>
2021-08-08 15:29:13 +0100

    q to quit

--- src/input.c
+++ src/input.c
@@ -2,9 +2,27 @@
 
 int checkQuit()
 {
+    int quit = 0;
     SDL_Event event;
 
-    if (SDL_PollEvent(&event) && event.type == SDL_QUIT) return 1;
+    if (SDL_PollEvent(&event))
+    {
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                quit = 1;
+                break;
 
-    return 0;
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_q)
+                {
+                    quit = 1;
+                }
+
+            default:
+                break;
+        }
+    }
+
+    return quit;
 }
